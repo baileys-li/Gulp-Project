173e532dee7c48a8b50bec132b7d00cce50d3c47
Arthur <arthurlitovko@gmail.com>
2019-12-04 11:51:47 +0200

    First gulp task (style)

--- gulpfile.js
+++ gulpfile.js
@@ -2,4 +2,12 @@ const   gulp  = require('gulp'),
         sass  = require('gulp-sass'),
         browserSync  = require('browser-sync'),
         pug  = require('gulp-pug');
-        
+
+function style() {
+    return gulp.src('./src/sass/**/*.sass')
+            .pipe(sass())
+            .pipe(gulp.dest('./public/css'))
+
+}
+
+exports.style = style;

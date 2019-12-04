21d286ab43af28361be095df9618ac404f610a02
Arthur <arthurlitovko@gmail.com>
2019-12-04 15:42:11 +0200

    Another gulp tasks style

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const   gulp  = require('gulp'),
         sass  = require('gulp-sass'),
         browserSync  = require('browser-sync').create(),
@@ -8,31 +10,28 @@ const   gulp  = require('gulp'),
  * Compile .pug files 
  */
 
-function page() {
-    return gulp.src('./src/*.pug')
+ gulp.task('pug', function () {
+     return gulp.src('./src/pages/*.pug')
             .pipe(pug({
                 pretty: true
               }))
             .pipe(gulp.dest("./public"))
-}
-  
-
-function style() {
+ })
+/**
+ * Compile .sass files 
+ */
+gulp.task('sass', function () {
     return gulp.src('./src/sass/**/*.sass')
             .pipe(sass())
             .pipe(gulp.dest('./public/css'))
-            .pipe(browserSync.stream())
+})
 
-}
+// function watch() {
+//     browserSync.init({
+//         baseDir: "./public"
+//     });
+//     gulp.watch('./src/*.pug', page);
+//     gulp.watch('./src/sass/**/*.sass', style);
+// }
 
-function watch() {
-    browserSync.init({
-        baseDir: "./public/"
-    });
-    gulp.watch('./src/*.pug', page);
-    gulp.watch('./src/sass/**/*.sass', style);
-}
-
-exports.style = style;
-exports.page = page;
-exports.watch = watch;
+// exports.watch = watch;

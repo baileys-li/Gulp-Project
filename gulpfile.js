228078f78fb9a5766deb517b3d7bb4efb1633e17
Arthur <arthurlitovko@gmail.com>
2019-12-13 22:43:30 +0200

    Test gulp pug bem plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,51 @@
-'use strict';
+"use strict";
 
-const   gulp  = require('gulp'),
-        sass  = require('gulp-sass'),
-        browserSync  = require('browser-sync').create(),
-        pug  = require('gulp-pug');
+const gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync").create(),
+  pug = require("gulp-pug"),
+  pugbem = require("gulp-pugbem");
 
+//** Compile .pug files **/
+gulp.task("pug", function() {
+  return gulp
+    .src("./src/pages/*.pug")
+    .pipe(
+      pug({
+        pretty: true,
+        plugins: [pugbem]
+      })
+    )
+    .pipe(gulp.dest("./public"));
+});
 
-/**
- * Compile .pug files 
- */
+//** Compile .sass files **/
 
- gulp.task('pug', function () {
-     return gulp.src('./src/pages/*.pug')
-            .pipe(pug({
-                pretty: true
-              }))
-            .pipe(gulp.dest("./public"))
- })
-/**
- * Compile .sass files 
- */
-gulp.task('sass', function () {
-    return gulp.src('./src/sass/*.sass')
-            .pipe(sass())
-            .pipe(gulp.dest('./public/css'))
-            .pipe(browserSync.stream());
-        })
+gulp.task("sass", function() {
+  return gulp
+    .src("./src/sass/*.sass")
+    .pipe(sass())
+    .pipe(gulp.dest("./public/css"))
+    .pipe(browserSync.stream());
+});
 
-gulp.task('watch', function () {
-    gulp.watch('./src/**/**/*.pug', gulp.series('pug'));
-    gulp.watch('./src/sass/**/*.sass', gulp.series('sass'));
-})
+//** Other tasks **/
 
-gulp.task('serve', function () {
-    browserSync.init({
-        server: {
-            baseDir: './public'
-        }
-    });
-    browserSync.watch('public', browserSync.reload);
-})
+gulp.task("watch", function() {
+  gulp.watch("./src/**/**/*.pug", gulp.series("pug"));
+  gulp.watch("./src/sass/**/*.sass", gulp.series("sass"));
+});
 
-gulp.task('default', gulp.series(
-    gulp.parallel('pug','sass'),
-    gulp.parallel('watch','serve')
-))
+gulp.task("serve", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./public"
+    }
+  });
+  browserSync.watch("public", browserSync.reload);
+});
+
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("pug", "sass"), gulp.parallel("watch", "serve"))
+);

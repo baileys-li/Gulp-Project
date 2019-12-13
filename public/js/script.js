fb59fcf46b2f68264f0f3c9073dff41b093f5c61
Arthur <arthurlitovko@gmail.com>
2019-12-13 19:08:19 +0200

    First slider

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+// * Open Hamburger Menu *
+
 var navMain = document.querySelector(".main-nav");
 var navToggle = navMain.querySelector(".main-nav__toggle");
 
@@ -11,4 +13,31 @@ navToggle.addEventListener("click", function() {
     navMain.classList.add("main-nav--closed");
     navMain.classList.remove("main-nav--opened");
   }
+});
+
+// * Slider *
+var advantages = document.querySelector(".advantages");
+
+var slider = advantages.querySelector(".slider");
+var slides = slider.querySelectorAll(".slider__item");
+var toggles = slider.querySelectorAll(".slider__toggle");
+
+slides.forEach(function(item) {
+  item.classList.remove("slider__item--nojs");
+});
+
+function showSlide(i) {
+  slides[i].classList.add("slider__item--active");
+  var prev = i > 0 ? i - 1 : slides.length - 1;
+  slides[prev].classList.remove("slider__item--active");
+  toggles[prev].classList.remove("slider__toggle--active");
+}
+
+showSlide(0);
+
+toggles.forEach(function(item, i, array) {
+  item.addEventListener("click", function() {
+    item.classList.add("slider__toggle--active");
+    showSlide(i);
+  });
 });
